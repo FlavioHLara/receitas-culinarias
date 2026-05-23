bcf81fc2a7e7af6482ad29af977f12eda9a1f0ee
FlavioHLara <144696891+FlavioHLara@users.noreply.github.com>
2026-05-23 01:01:08 -0300

    feat: swagger das rotas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import authRoutes from './routes/auth.routes';
 import receitaRoutes from "./routes/receita.routes";
 import categoriaRoutes from './routes/categoria.routes';
 import { errorHandler } from './middlewares/errorHandler';
+import { swaggerSpec } from './config/swagger';
 
 const app: Application = express();
 
@@ -12,7 +14,10 @@ app.use(express.json());
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
-})
+});
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/auth', authRoutes);
 app.use('/receita', receitaRoutes);
 app.use('/categoria', categoriaRoutes);
